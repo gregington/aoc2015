30558d438e87c36fbc966c0e5150bbd2258829c4
Greg Tan <greg.tan@firstmode.com>
2023-12-10 11:35:49 +0800

    Day 19 part 1

---
+++ aoc19/Program.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections.Frozen;
+using System.Collections.Immutable;
+using System.CommandLine;
+using System.CommandLine.Parsing;
+using System.Runtime.InteropServices;
+
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var inputOption = new Option<string>(
+            name: "--input",
+            description: "Input file path",
+            getDefaultValue: () => "input.txt");
+
+        var partOption = new Option<int>(
+            name: "--part",
+            description: "Part of the puzzle to solve",
+            getDefaultValue: () => 1);
+
+        var rootCommand = new RootCommand();
+        rootCommand.AddOption(inputOption);
+        rootCommand.AddOption(partOption);
+
+        rootCommand.SetHandler(Run, inputOption, partOption);
+
+        await rootCommand.InvokeAsync(args);
+    }
+
+    private async static Task Run(string input, int part)
+    {
+        var (replacements, molecule) = await Parse(input);
+        var task = part switch
+        {
+            1 => Part1(replacements, molecule),
+            2 => Part2(replacements, molecule),
+            _ => throw new ArgumentException($"Invalid part: {part}", nameof(part))
+        };
+
+        await task;
+    }
+
+    public static Task Part1(FrozenDictionary<string, ImmutableArray<string>> replacements, string molecule)
+    {
+        var replacedCount = replacements.SelectMany(kvp => kvp.Value.SelectMany(r => Replace(molecule, kvp.Key, r)))
+            .Distinct()
+            .Count();
+
+        Console.WriteLine(replacedCount);
+
+        return Task.CompletedTask;
+    }
+
+    public static Task Part2(FrozenDictionary<string, ImmutableArray<string>> replacements, string molecule)
+    {
+        return Task.CompletedTask;
+    }
+
+    private static IEnumerable<string> Replace(string molecule, string find, string replace)
+    {
+        var startIndex = 0;
+        while (true)
+        {
+            var i = molecule.IndexOf(find, startIndex);
+            if (i == -1)
+            {
+                break;
+            }
+
+            var replacement = string.Concat(molecule.AsSpan(0, i), replace, molecule.AsSpan(i + find.Length));
+            yield return replacement;
+            startIndex = i + 1;
+        }
+    }
+
+    private static void PrintReplacements(IReadOnlyDictionary<string, ImmutableArray<string>> replacements)
+    {
+        foreach(var kvp in replacements.OrderBy(kvp => kvp.Key))
+        {
+            Console.WriteLine($"{kvp.Key, 5} => {string.Join(", ", kvp.Value)}");
+        }
+    }
+
+    public static async Task<(FrozenDictionary<string, ImmutableArray<string>> Replacements, string molecule)> Parse(string input)
+    {
+        var replacements = new Dictionary<string, ImmutableArray<string>>();
+     
+        var lines = await File.ReadAllLinesAsync (input);     
+        foreach (var line in lines)
+        {
+            if (line.Trim() == string.Empty)
+            {
+                continue;
+            }
+
+            if (line.Contains("=>")) {
+                var parts = line.Split("=>", StringSplitOptions.TrimEntries);
+
+                var arr = replacements.TryGetValue(parts[0], out var value) ? value : [];
+                replacements[parts[0]] = arr.Add(parts[1]);
+
+                continue;
+            }
+
+            return (replacements.ToFrozenDictionary(), line);
+        }
+        throw new Exception("Could not parse input");
+    }
+}
